2f94271cc3974d6fb0c26cc86ff6348ee0db62d7
jana_jo <Joanna.Janas@dlr.de>
2016-11-30 16:48:20 +0100

    Add useful converters

---
+++ spatz_boost_awk_code.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+awk 'BEGIN{expo = 0;} { if (NR>14 && NR%2 == 1) expo = $1; else if(NR>14 && NR%2 == 0) {gsub(",",".",$0); gsub("·10","",$0); printf("%d, %f, %f\n", $1, (float)$2 * (10 ^ expo), $3) > "mod_tab3.txt"}}' Tabelle_3.txt
